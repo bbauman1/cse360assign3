fe2d66c3c3a51d0ccaff77a756688c7b4a189d99
Brett Bauman <bauman.brett3@gmail.com>
2016-02-20 18:14:02 -0700

    Initial commit of default JUnit tests

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,67 @@
+package cse360assign3;
+/**
+ * Calculator class that can perform the 4 basic math operations. History of operations
+ * is recorded.
+ * @author Brett Bauman
+ * PIN: 117
+ *
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Creates an integer total
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Get the total value
+	 * @return	Value of total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds parameter to total
+	 * @param value integer to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts parameter from total
+	 * @param value integer to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies parameter to total
+	 * @param value integer to be multiplied
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides parameter from total
+	 * @param value integer to be divided by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Creates a string from history of actions. Contents are separated by normal operators
+	 * @return String history of actions
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
